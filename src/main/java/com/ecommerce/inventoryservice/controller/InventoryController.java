4bdcbb791c4b4aad1f89e1d6881704eebe6464ff
ravindar86 <ravindar.mca@gmail.com>
2022-01-03 16:16:46 +0530

    Commit changes for log trace (Sleuth & Zip) and logstash (ELK stack)

--- src/main/java/com/ecommerce/inventoryservice/controller/InventoryController.java
+++ src/main/java/com/ecommerce/inventoryservice/controller/InventoryController.java
@@ -2,12 +2,14 @@ package com.ecommerce.inventoryservice.controller;
 
 import com.ecommerce.inventoryservice.model.Inventory;
 import com.ecommerce.inventoryservice.repository.InventoryRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/inventory")
 public class InventoryController {
 
@@ -24,6 +26,8 @@ public class InventoryController {
 
     @GetMapping("/{skuCode}")
     public Boolean isStockAvailable(@PathVariable String skuCode){
+        log.info("Checking stock availability for product {}", skuCode);
+
         Inventory inventory = inventoryRepository.findBySkuCode(skuCode)
                                     .orElseThrow(() -> new RuntimeException("Cannot find inventory for "+skuCode));
 
